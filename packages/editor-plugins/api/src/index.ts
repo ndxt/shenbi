8c76272074ad3b2b645c258de0f47a75d03bf9b1
张凯 <zk@centit.com>
2026-03-22 19:22:30 +0800

    refactor: narrow plugin api root exports

--- packages/editor-plugins/api/src/index.ts
+++ packages/editor-plugins/api/src/index.ts
@@ -1,3 +1,21 @@
-export * from './context';
+export type {
+  PluginCommandService,
+  PluginWorkspaceService,
+  PluginPersistenceService,
+  PluginNotifications,
+  PluginSelectionService,
+  PluginDocumentPatchService,
+  PluginDocumentService,
+  PluginFileSystemService,
+  PluginContext,
+} from './context';
+export {
+  getPluginSchema,
+  getPluginDocumentPatchService,
+  replacePluginSchema,
+  getPluginSelectedNodeId,
+  executePluginCommand,
+  getPluginNotifications,
+} from './context';
 export * from './contributions';
 export * from './plugin';
